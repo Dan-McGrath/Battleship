21fe98450f12e6816878b3d3c68cc1420bd6204a
Dan McGrath <mcgrathd3254@yahoo.com>
2023-08-20 22:04:26 -0400

    Add func that test if ship can be placed in a certain direction

--- src/components/Gameboard Object/gameboardObject.js
+++ src/components/Gameboard Object/gameboardObject.js
@@ -1,3 +1,5 @@
+import ship from "../Ship Object/shipObject";
+
 const gameboard = () => {
   const gameboardArr = [];
   const createGameboard = () => {
@@ -19,22 +21,83 @@ const gameboard = () => {
 
   const getGameBoardArray = () => gameboardArr;
 
-  const placeShip = (cordinate, newShip) => {
+  const checkCordinates = (cordinate) => {
     const board = getGameBoardArray();
     let square;
-    const ship = newShip.getLength();
     for (let i = 0; i < board.length; i++) {
       if (JSON.stringify(board[i].cord) === JSON.stringify(cordinate)) {
         square = board[i];
         if (square.isOccupied === false);
-        square.isOccupied = true;
-        return { square, ship };
+        return true;
       }
     }
-    return { square, ship };
+    return false;
   };
 
-  return { createGameboard, getGameBoardArray, placeShip };
+  const placeShipHorizontalyRight = (cordinate, newShip) => {
+    const shipLength = newShip.getLength();
+    let cordinateToChange = cordinate[1];
+    const newCordinates = [];
+    for (let i = 0; i < shipLength; i++) {
+      cordinateToChange = cordinate[1];
+      cordinateToChange += i;
+      const newCordinate = [cordinate[0], cordinateToChange];
+      newCordinates.push(newCordinate);
+    }
+    return newCordinates.every((ele) => checkCordinates(ele));
+  };
+
+  const placeShipHorizontalyLeft = (cordinate, newShip) => {
+    const shipLength = newShip.getLength();
+    let cordinateToChange = cordinate[1];
+    const newCordinates = [];
+    for (let i = shipLength; i >= 0; i--) {
+      cordinateToChange = cordinate[1];
+      cordinateToChange -= i;
+      const newCordinate = [cordinate[0], cordinateToChange];
+      newCordinates.push(newCordinate);
+    }
+    return newCordinates.every((ele) => checkCordinates(ele));
+  };
+
+  const placeShipVerticalyUp = (cordinate, newShip) => {
+    const shipLength = newShip.getLength();
+    let cordinateToChange = cordinate[0];
+    const newCordinates = [];
+    for (let i = shipLength; i >= 0; i--) {
+      cordinateToChange = cordinate[0];
+      cordinateToChange -= i;
+      const newCordinate = [cordinateToChange, cordinate[1]];
+      newCordinates.push(newCordinate);
+    }
+    return newCordinates.every((ele) => checkCordinates(ele));
+  };
+
+  const placeShipVerticalyDown = (cordinate, newShip) => {
+    const shipLength = newShip.getLength();
+    let cordinateToChange = cordinate[0];
+    const newCordinates = [];
+    for (let i = 0; i < shipLength; i++) {
+      cordinateToChange = cordinate[0];
+      cordinateToChange += i;
+      const newCordinate = [cordinateToChange, cordinate[1]];
+      newCordinates.push(newCordinate);
+    }
+    return newCordinates.every((ele) => checkCordinates(ele));
+  };
+
+  const placeShip = (cordinate, newShip) => {};
+
+  return {
+    createGameboard,
+    getGameBoardArray,
+    placeShip,
+    checkCordinates,
+    placeShipHorizontalyRight,
+    placeShipHorizontalyLeft,
+    placeShipVerticalyUp,
+    placeShipVerticalyDown
+  };
 };
 
 export default gameboard;
--- src/components/Gameboard Object/gameboardObject.test.js
+++ src/components/Gameboard Object/gameboardObject.test.js
@@ -15,9 +15,76 @@ test("should create gameboard with a cordinate of [9,9]", () => {
   expect(result).toStrictEqual(target);
 });
 
-test("should place ship at cord [0, 0]", () => {
+test("should check if square is occupied", () => {
+  const shipCords = [0, 0];
+  const shipPlacement = newGameboard.checkCordinates(shipCords);
+  expect(shipPlacement).toBe(true);
+});
+
+test("should check if ship can be placed horizontaly to the right", () => {
   const newShip = ship(3);
   const shipCords = [0, 0];
-  const shipPlacement = newGameboard.placeShip(shipCords, newShip);
+  const shipPlacement = newGameboard.placeShipHorizontalyRight(
+    shipCords,
+    newShip
+  );
   expect(shipPlacement).toBe(true);
+});
+
+test("should return false if ship cannot be placed horizontaly to the right", () => {
+  const newShip = ship(3);
+  const shipCords = [0, 9];
+  const shipPlacement = newGameboard.placeShipHorizontalyRight(
+    shipCords,
+    newShip
+  );
+  expect(shipPlacement).toBe(false);
+});
+
+test("should check if ship can be placed horizontaly to the left", () => {
+  const newShip = ship(3);
+  const shipCords = [0, 4];
+  const shipPlacement = newGameboard.placeShipHorizontalyLeft(
+    shipCords,
+    newShip
+  );
+  expect(shipPlacement).toBe(true);
+});
+
+test("should return false if ship can not be palced horizontally to the left", () => {
+  const newShip = ship(3);
+  const shipCords = [0, 0];
+  const shipPlacement = newGameboard.placeShipHorizontalyLeft(
+    shipCords,
+    newShip
+  );
+  expect(shipPlacement).toBe(false);
+});
+
+test("should check if ship can be placed vertically up", () => {
+  const newShip = ship(3);
+  const shipCords = [3, 0];
+  const shipPlacement = newGameboard.placeShipVerticalyUp(shipCords, newShip);
+  expect(shipPlacement).toBe(true);
+});
+
+test("should return false if ship can not be placed vertically up", () => {
+  const newShip = ship(3);
+  const shipCords = [0, 0];
+  const shipPlacement = newGameboard.placeShipVerticalyUp(shipCords, newShip);
+  expect(shipPlacement).toBe(false);
+});
+
+test("should check if ship can be place vertically down", () => {
+  const newShip = ship(3);
+  const shipCords = [0, 0];
+  const shipPlacement = newGameboard.placeShipVerticalyDown(shipCords, newShip);
+  expect(shipPlacement).toBe(true);
+});
+
+test("should return false if ship can not be placed vertically down", () => {
+  const newShip = ship(3);
+  const shipCords = [9, 0];
+  const shipPlacement = newGameboard.placeShipVerticalyDown(shipCords, newShip);
+  expect(shipPlacement).toBe(false);
 });
