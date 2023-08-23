709541da15ab865646792227600c991cc9f1b0c1
Dan McGrath <mcgrathd3254@yahoo.com>
2023-08-22 22:31:38 -0400

    add function to get number of hits

--- src/components/Ship Object/shipObject.js
+++ src/components/Ship Object/shipObject.js
@@ -1,22 +1,26 @@
 const ship = (length, name) => {
   let numberOfHits = 0;
   let sunk = false;
-
+  const getNumberofHits = () => numberOfHits;
   const getName = () => name;
   const getLength = () => length;
-
-  const hit = () => {
-    numberOfHits++;
-    return numberOfHits;
-  };
 
   const isSunk = () => {
     if (numberOfHits >= getLength()) {
       sunk = true;
     }
-    return sunk;
+    return false;
   };
-  return { hit, isSunk, getLength, getName };
+
+  const hit = () => {
+    numberOfHits++;
+    if (numberOfHits >= getLength()) {
+      isSunk();
+    }
+    return numberOfHits;
+  };
+
+  return { hit, isSunk, getLength, getName, getNumberofHits };
 };
 
 export default ship;
