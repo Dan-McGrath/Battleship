fe541eb6a4d4709db24664777535ec7f4023a472
Dan McGrath <mcgrathd3254@yahoo.com>
2023-08-23 00:25:41 -0400

    add selections to place ship

--- src/components/Player Object/playerObject.js
+++ src/components/Player Object/playerObject.js
@@ -30,9 +30,73 @@ const player = (name) => {
   gameboardObject.createGameboard();
   const playersGameboard = gameboardObject.getGameBoardArray();
 
+  const directionUp = gameboardObject.placeShipVerticalyUp;
+  const directionDown = gameboardObject.placeShipVerticalyDown;
+  const directionRight = gameboardObject.placeShipHorizontalyRight;
+  const directionLeft = gameboardObject.placeShipHorizontalyLeft;
+
   const attack = (cordinates) => cordinates;
 
-  return { getName, ships, playersGameboard, attack, gameboardObject };
+  const selectShip = (index) => {
+    if (ships[index].placed === true) {
+      ships[index].placed = false;
+    }
+    return ships[index].ship;
+  };
+
+  const selectCordinates = (index) => {
+    if (playersGameboard[index].isOccupied === true) {
+      return false;
+    }
+    return playersGameboard[index].cord;
+  };
+
+  const selectDirection = (direction) => {
+    if (direction === "up") {
+      return directionUp;
+    }
+    if (direction === "down") {
+      return directionDown;
+    }
+    if (direction === "right") {
+      return directionRight;
+    }
+    if (direction === "left") {
+      return directionLeft;
+    }
+    return false;
+  };
+
+  const placeShip = (shipIndex, direction, cordinatesIndex) => {
+    const cordinate = selectCordinates(cordinatesIndex);
+    const playerdirection = selectDirection(direction);
+    const playerShip = selectShip(shipIndex);
+    if (ships[shipIndex].placed === "false") {
+      if (cordinate === false) {
+        return false;
+      }
+
+      if (playerdirection === false) {
+        return false;
+      }
+      console.log(
+        gameboardObject.placeShip(playerdirection, playerShip, cordinate)
+      );
+      return gameboardObject.placeShip(playerdirection, playerShip, cordinate);
+    }
+    return true;
+  };
+  return {
+    getName,
+    ships,
+    playersGameboard,
+    attack,
+    gameboardObject,
+    selectShip,
+    selectDirection,
+    selectCordinates,
+    placeShip
+  };
 };
 
 export default player;
