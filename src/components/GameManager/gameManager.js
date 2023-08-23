761f97d14f8a7fee60b879aa88a7f98753edbf2b
Dan McGrath <mcgrathd3254@yahoo.com>
2023-08-23 00:25:16 -0400

    Simplfied object

--- src/components/GameManager/gameManager.js
+++ src/components/GameManager/gameManager.js
@@ -4,27 +4,14 @@ const gameManager = () => {
   const player1 = player("Player 1");
   const player2 = player("Player 2");
 
+  const currentPlayer = player1;
+
   const player1Ships = player1.ships;
   const player2Ships = player2.ships;
-  const start = () => {
-    const player1PlaceShips = () => {
-      player1Ships.forEach((ele) => {
-        while (ele.placed === false) {
-          const getShip = (shipIndex) => {
-            player1Ships[shipIndex];
-          };
-          const getDirection = (direction) => {};
-          // const getShipCordinate =;
-          player1.gameboardObject.placeShip(
-            getDirection,
-            getShip
-            // shipCordinate
-          );
-        }
-      })();
-    };
-  };
-  return { start, player1, player2 };
+
+  const placeShips = () => {};
+
+  return { currentPlayer };
 };
 
 export default gameManager;
